1dd50e550d82d89a8f25fd8304fa1bd177155d49
Vamsi <gvamsi1113@gmail.com>
2023-07-07 18:00:20 +0530

    Mouse-pointer-addition

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const track = document.getElementById("image-track");
+const brat = document.getElementById("with-mouse");
+
 
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
@@ -10,6 +12,13 @@ window.onmouseup = e => {
 }
 
 window.onmousemove = e => {
+    const mX = e.clientX;
+    const mY = e.clientY;
+    
+    brat.animate({
+        transform: `translate(${mX - 15}px, ${mY - 15}px) rotate(${(mX-mY)*2.5}deg)`
+    }, { duration: 600, fill: "forwards" });
+
     if (track.dataset.mouseDownAt === "0") return;
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
@@ -19,7 +28,7 @@ window.onmousemove = e => {
     const yDelta = ((e.clientY - window.innerHeight / 2) / window.innerHeight * 100);
 
     track.animate({
-        transform: `translate(${nextPercentage-8}%, ${yDelta * 1 / 5 - 50}%)`
+        transform: `translate(${nextPercentage - 8}%, ${yDelta * 1 / 5 - 50}%)`
     }, { duration: 1200, fill: "forwards" });
 
     track.dataset.percentage = nextPercentage;
@@ -27,6 +36,15 @@ window.onmousemove = e => {
     for (const image of track.getElementsByClassName("image")) {
         image.animate({
             objectPosition: `${nextPercentage + 100}% center`
-        }, { duration: 1500, fill: "forwards" });
+        }, { duration: 1200, fill: "forwards" });
     }
 }
+
+// window.onmousemove = wme => {
+//     const mX = wme.clientX/window.innerWidth*100;
+//     const mY = wme.clientY/window.innerHeight*100;
+
+//     brat.animate({
+//         transform: `translate(${mX}%, ${mY}%)`
+//     }, { duration: 1200, fill: "forwards" });
+// }
