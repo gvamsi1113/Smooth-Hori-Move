08bde87218d255a3c534142b2347e99926d9bbe9
Vamsi <gvamsi1113@gmail.com>
2023-08-03 02:12:14 -0700

    properified the existing and added a wrapper

--- index.js
+++ index.js
@@ -19,10 +19,17 @@ window.onmousemove = e => {
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
 
-    const nextPercentage = Math.min(Math.max(parseFloat(track.dataset.prevPercentage) + mouseDelta / maxDelta * -100, -84), 0);
+
+
+    // const nextPercentage = Math.min(Math.max(parseFloat(track.dataset.prevPercentage) + mouseDelta / maxDelta * -100, -63), 0);
+
+    // const limitConst = 41+41+7*3+8*18 = 248;  track(paddingLeft + paddingRight) + (number of gaps * width of each gap) + (number of elements * width of each)
+    // So for maxDelta movement of mouse the track should move 248 and should not move more than that so limited to that amount.
+
+    const nextPercentage = Math.min(Math.max(parseFloat(track.dataset.prevPercentage) + mouseDelta / maxDelta * -100, -100+100/2.48), 0);
 
     track.animate({
-        transform: `translate(${nextPercentage - 8}%, -50%)`
+        transform: `translate(${nextPercentage}%, -50%)`
     }, { duration: 1500, fill: "forwards" });
 
     track.dataset.percentage = nextPercentage;
