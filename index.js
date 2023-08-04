132ac1d3780edadd2505cbc57d6fb3c154c8a166
Vamsi <gvamsi1113@gmail.com>
2023-08-04 01:28:45 -0700

    Resticted draggable area to projects area

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const track = document.getElementById("image-track");
-const brat = document.getElementById("with-mouse");
 
 
 window.onmousedown = e => {
     track.dataset.mouseDownAt = e.clientX;
+    if (e.clientY >= window.innerHeight / 2 + 30 / 2 * window.innerWidth / 100 || e.clientY <= window.innerHeight / 2 - 30 / 2 * window.innerWidth / 100) track.dataset.mouseDownAt = 0;
+    // if statement deregisters the e.clientX if the mousedown happens outside the middle area
 }
 
 window.onmouseup = e => {
     track.dataset.mouseDownAt = "0";
     track.animate({
-        transform: `translate(${Math.round(track.dataset.percentage/19.5*1.545)*19.5/1.545}%, -50%)`
-    }, { duration: 800, fill: "forwards" });
+        transform: `translate(${Math.round(track.dataset.percentage / 19.5 * 1.545) * 19.5 / 1.545}%, -50%)`
+    }, { duration: 600, fill: "forwards" });
     for (const image of track.getElementsByClassName("image")) {
         image.animate({
-            objectPosition: `${Math.round(track.dataset.percentage/19.5*1.545)*19.5/1.545 + 100}% center`
-        }, { duration: 800, fill: "forwards" });
+            objectPosition: `${Math.round(track.dataset.percentage / 19.5 * 1.545) * 19.5 / 1.545 + 100}% center`
+        }, { duration: 500, fill: "forwards" });
     }
-    
-    track.dataset.prevPercentage = Math.round(track.dataset.percentage/19.5*1.545)*19.5/1.545;
+
+    track.dataset.prevPercentage = Math.round(track.dataset.percentage / 19.5 * 1.545) * 19.5 / 1.545;
 }
 
 window.onmousemove = e => {
-    const mX = e.clientX;
-    const mY = e.clientY;
 
     if (track.dataset.mouseDownAt === "0") return;
 
@@ -35,7 +34,7 @@ window.onmousemove = e => {
     // const limitConst = 41+41+7*3+8*18 = 247;  track(paddingLeft + paddingRight) + (number of gaps * width of each gap) + (number of elements * width of each)
     // So for maxDelta movement of mouse the track should move 248 and should not move more than that so limited to that amount.
 
-    const nextPercentage = Math.min(Math.max(parseFloat(track.dataset.prevPercentage) + mouseDelta / maxDelta * -100, -100+18*100/154.5), 0);
+    const nextPercentage = Math.min(Math.max(parseFloat(track.dataset.prevPercentage) + mouseDelta / maxDelta * -100, -100 + 18 * 100 / 154.5), 0);
 
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
